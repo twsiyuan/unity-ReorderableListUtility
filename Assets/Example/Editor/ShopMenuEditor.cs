8d58e9ec8afc65d5ab73662869947d0c18122c03
easy <sy.wang@innojoy.com.tw>
2016-06-29 16:55:12 +0800

    Main code and example

---
+++ Assets/Example/Editor/ShopMenuEditor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEditor;
+using UnityEditorInternal;
+
+[CustomEditor(typeof(ShopMenu))]
+[CanEditMultipleObjects]
+public class ShopMenuEditor : Editor
+{
+    ReorderableList list1;
+    ReorderableList list2;
+
+    void OnEnable()
+    {
+        var property = this.serializedObject.FindProperty("Drinks");
+
+        this.list1 = ReorderableListUtility.CreateAutoLayout(property);
+        this.list2 = ReorderableListUtility.CreateAutoLayout(
+            property, 
+            new string[] { "Drink Name", "Cost", "Key Color" },
+            new float?[] { 100, 70 }
+        );
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        this.serializedObject.Update();
+
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Example1", EditorStyles.boldLabel);
+        ReorderableListUtility.DoLayoutListWithFoldout(this.list1);
+
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Example2", EditorStyles.boldLabel);
+        ReorderableListUtility.DoLayoutListWithFoldout(this.list2);
+
+        this.serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/Example/Scripts/ShopMenu.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ShopMenu : MonoBehaviour
+{
+    public Drink[] Drinks = new Drink[0];
+
+    [System.Serializable]
+    public struct Drink
+    {
+        public string Name;
+        public float Price;
+        public Color Color;
+    }
+
+    void Reset()
+    {
+        var list = new List<Drink>();
+
+        {
+            var d = new Drink();
+            d.Name = "Darjeeling";
+            d.Price = 10.99f;
+            list.Add(d);
+        }
+
+        {
+            var d = new Drink();
+            d.Name = "Assam";
+            d.Price = 9.99f;
+            list.Add(d);
+        }
+
+        {
+            var d = new Drink();
+            d.Name = "Orange Pekoe";
+            d.Price = 8.99f;
+            list.Add(d);
+        }
+
+        {
+            var d = new Drink();
+            d.Name = "Rose Hip";
+            d.Price = 7.99f;
+            list.Add(d);
+        }
+
+        this.Drinks = list.ToArray();
+    }
+}
